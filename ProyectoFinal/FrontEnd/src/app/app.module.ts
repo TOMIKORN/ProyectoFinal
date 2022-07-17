116c2adb344eecc62279df98bb4f097d39ac2c0e
TomasGimenez <tomassantiagogimenez@hotmail.com.ar>
2022-07-17 05:44:20 -0300

    Cerrar Sesion - Interceptor

--- ProyectoFinal/FrontEnd/src/app/app.module.ts
+++ ProyectoFinal/FrontEnd/src/app/app.module.ts
@@ -17,6 +17,7 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule
     
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
